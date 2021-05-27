083aa19cd2be799f706b2f28094a6f5b80a6bc7f
Malcolm <malcolm.haddon@gmail.com>
2021-05-27 10:34:36 +1000

    encapsulated the main MSE within do_MSE, No EWN

---
+++ R/domse.R
@@ -0,0 +1,145 @@
+
+
+
+#' @title do_MSE an ecapsulating function to hold the details of a single run
+#'
+#' @description do_MSE is a form of meta-function that contains the code and
+#'     function calls that constitute a single scenario run for aMSE. This is
+#'     the reason the argument list is as long as it is. The code has been
+#'     encapsulated like this to simplify development, maintenance, and
+#'     distribution. Already with the number of variant conditioning examples
+#'     produced as tests of the code-base errors have arisen through a failure
+#'     to propagate all changes to the code now in this function to all the
+#'     separate MSE run files used to manage each scenario. A potential flaw
+#'     lies with the need to apply the jurisdictionHS functions to any
+#'     histirical fishery data used to condition the operating model, ready for
+#'     the first year of the projections. Currently, the code used is unique to
+#'     the Tasmanian case and this obviously still requires generalization.
+#'     This may require a new function to be produced in each jurisdictionHS.R
+#'     file, but that is still under consideration. Part of the development of
+#'     this meta-function will be to further articulate the generation of
+#'     results, and the inclusion of error traps on the argument list entries.
+#'
+#' @param rundir the full path to the directory in which all files relating to a
+#'     particular run are to be held. If datadir != rundir then the main data
+#'     files are kept in datadir. The juridictionHS.R can be held in either.
+#' @param controlfile the filename of the control file present in rundir
+#'     containing information regarding the particular run.
+#' @param datadir the directory in which the SAU data file is to be found. This
+#'     will usually be the rundir for the scenario run but if the data file is
+#'     to be shared among a set of scenarios it will be more efficient to define
+#'     a separate datadir
+#' @param hsargs the constants used to define the workings of the hcr
+#' @param hcrfun the name of the harvest control rule that is used to
+#'     calculate the aspirational catches by SAU or TAC by zone for the
+#'     following year
+#' @param sampleCE a function from jurisdictionHS.R that generates the CPUE
+#'     statistics from the projected data
+#' @param sampleFIS a function from jurisdictionHS.R that generates the FIS
+#'     statistics
+#' @param sampleNaS a function from jurisdictionHS.R that generates the
+#'     Numbers-at-size samples
+#' @param getdata a function that gathers all the data required by the hcrfun
+#'     and combines it into an hcrdata object ready for the hcrfun
+#' @param calcpopC a function that takes the output from hcrfun (either the
+#'     aspirational catch x SAU or TAC x zone) and generates the actual catch
+#'     per population in each SAU expected in the current year.
+#' @param varyrs how many years at the end of the conditioning on the fishery,
+#'     data into zoneDD, to which to add recruitment variation, default = 7,
+#'     which suits the Tasmanian west coast. Used in prepareprojection
+#' @param startyr the index for the first year of the conditioning dynamics to
+#'     include in the plotted cpue trajectories, to give startyr:indexoflastyear.
+#'     used in sauplots.
+#' @param cleanslate should the directory be emptied of all files first? All
+#'     html, png, RData, and css files in the directory will be deleted.
+#'     default = FALSE. This is obviously a very powerful and potentially
+#'     dangerous argument, hence it needs to be set =TRUE explicitly. It does
+#'     not delete any .csv files so if the rundir is used to store the data for
+#'     the run then 'cleanslate' will not affect the data or any .R files.
+#' @param verbose Should progress comments be printed to console, default=FALSE
+#' @param doproject should the projections be run. default = TRUE. When
+#'     conditioning the operating model this should be set to FALSE to focus
+#'     the program on the production of the completely conditioned zone and its
+#'     outputs.
+#' @param ndiagprojs the number of replicate trajectories to plot in the
+#'     diagnostics tab to illustrate ndiagprojs trajectories to ensure that
+#'     such projections appear realistic; default=3
+#'
+#' @return a large list containing tottime, projtime, starttime, glb, ctrl,
+#'     zoneDD, zoneDP, projC, condC, sauout, and outzone
+#' @export
+#'
+#' @examples
+#' print("wait on suitable data sets in data")
+do_MSE <- function(rundir,controlfile,datadir,hsargs,hcrfun,sampleCE,sampleFIS,
+                   sampleNaS,getdata,calcpopC,varyrs=7,startyr=32,
+                   cleanslate=FALSE,verbose=FALSE,doproject=TRUE,ndiagprojs=3) {
+  # generate equilibrium zone ----------------------------------------------------
+  starttime <- (Sys.time())
+  zone <- makeequilzone(rundir,controlfile,datadir,
+                        cleanslate=cleanslate,verbose=verbose)
+  equiltime <- (Sys.time()); if (verbose) print(equiltime - starttime)
+  # declare main objects -------------------------------------------------------
+  glb <- zone$glb
+  ctrl <- zone$ctrl
+  zone1 <- zone$zone1
+  projC <- zone$zone1$projC
+  condC <- zone$zone1$condC
+  zoneC <- zone$zoneC
+  zoneD <- zone$zoneD
+  product <- zone$product
+  # save some equil results ----------------------------------------------------
+  biology_plots(rundir, glb, zoneC)
+  plotproductivity(rundir,product,glb)
+  numbersatsize(rundir, glb, zoneD)
+  #Condition on Fishery --------------------------------------------------------
+  if (verbose) cat("Conditioning on the Fishery data  \n")
+  zoneDD <- dohistoricC(zoneD,zoneC,glob=glb,condC,calcpopC=calcpopC,
+                        sigR=1e-08,sigB=1e-08)
+  hyrs <- glb$hyrs
+  propD <- t(getzoneprops(zoneC,zoneDD,glb,year=hyrs))
+  addtable(round(propD,4),"propertyDD.csv",rundir,category="zoneDD",caption=
+             "Properties of zoneD after conditioning on historical catches.")
+  addtable(round(t(zoneDD$harvestR[(hyrs-9):hyrs,]),4),"final_harvestR.csv",
+           rundir,category="zoneDD",
+           caption="Last ten years of population vs harvest rate.")
+  popdefs <- getlistvar(zone$zoneC,"popdef")
+  addtable(round(t(popdefs),3),"popdefs.csv",rundir,category="zoneDD",
+           caption="Population vs Operating model parameter definitions")
+  plotconditioning(zoneDD,glb,zoneC,condC$histCE,rundir)
+  # do projections ------------------------------------------------------------
+  if (doproject) {
+    if (verbose) cat("Doing the projections \n")
+    # uses Tasmania's HCR; generation of indata needs generalization
+    indata <- list(arrce=condC$histCE,yearnames=rownames(condC$histCE),
+                   acatches=tail(condC$histCatch,1),fis=NULL,nas=NULL)
+    cmcda <- hcrfun(indata,hsargs=hsargs,saunames=glb$saunames)
+    pms <- cmcda$pms
+    multTAC <- cmcda$multTAC
+    out <- prepareprojection(projC=projC,condC=condC,zoneC=zoneC,glb=glb,
+                             multTAC=multTAC,calcpopC=calcpopC,zoneDD=zoneDD,
+                             ctrl=ctrl,varyrs=varyrs,lastsigR = ctrl$withsigR)
+    zoneDP <- out$zoneDP
+    projC <- out$projC
+    zoneCP <- out$zoneCP
+    zoneDDR <- out$zoneDDR
+    zoneDP <- doprojections(ctrl,zoneDP,zoneCP,condC$histCE,glb,hcrfun=hcrfun,
+                            hsargs,sampleCE=sampleCE,sampleFIS=sampleFIS,
+                            sampleNaS=sampleNaS,getdata=getdata,calcpopC=calcpopC)
+    histCE <- condC$histCE
+    B0 <- getvar(zoneC,"B0")
+    ExB0 <- getvar(zoneC,"ExB0")
+    sauout <- sauplots(zoneDP,zoneDDR,glb,rundir,B0,ExB0,startyr=startyr,
+                       addCI=TRUE,histCE=histCE)
+    diagnosticsproj(sauout$zonePsau,glb,rundir,nrep=ndiagprojs)
+    outzone <- poptozone(zoneDP,glb,
+                         B0=sum(getvar(zoneC,"B0")),
+                         ExB0=sum(getvar(zoneC,"ExB0")))
+    plotZone(outzone,rundir,glb,CIprobs=c(0.05,0.5,0.95),addfile=TRUE)
+  }
+  projtime <- Sys.time()
+  tottime <- round((projtime - starttime),3)
+  out <- list(tottime=tottime,projtime=projtime,starttime=starttime,
+              glb=glb,ctrl=ctrl,zoneDD=zoneDD,zoneDP=zoneDP,projC=projC,
+              condC=condC,sauout=sauout,outzone=outzone)
+} # end of do_MSE
